1a61d4133e6ebc5ab1a056688ab84e06a895e89e
alansbarnes <alansbarnes630@hotmail.com>
2026-01-09 18:45:36 -0500

    Add dialog control IDs for properties in Resource.h
    Added new dialog control IDs for properties settings.

--- Resource.h
+++ Resource.h
@@ -4,6 +4,31 @@
 
 #define IDS_APP_TITLE			103
 
+#define IDM_PROPERTIES          40001
+#define IDD_PROPERTIES          201
+
+// Dialog control IDs
+#define IDC_MAX_ITER            1001
+#define IDC_MAX_ITER_SPIN       1002
+#define IDC_CENTER_REAL         1003
+#define IDC_CENTER_IMAG         1004
+#define IDC_HEIGHT              1005
+
+#define IDC_RED_MIN             1006
+#define IDC_RED_MIN_SPIN        1007
+#define IDC_RED_MAX             1008
+#define IDC_RED_MAX_SPIN        1009
+
+#define IDC_GREEN_MIN           1010
+#define IDC_GREEN_MIN_SPIN      1011
+#define IDC_GREEN_MAX           1012
+#define IDC_GREEN_MAX_SPIN      1013
+
+#define IDC_BLUE_MIN            1014
+#define IDC_BLUE_MIN_SPIN       1015
+#define IDC_BLUE_MAX            1016
+#define IDC_BLUE_MAX_SPIN       1017
+
 #define IDR_MAINFRAME			128
 #define IDD_MANDELBROT_DIALOG	102
 #define IDD_ABOUTBOX			103
